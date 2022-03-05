576398a492f19cb1354f2f09f62677e5724b9237
Bodunrin David <bodunrindavidbond@gmail.com>
2022-03-05 11:29:01 +0100

    Changed the bcrypt package to bcryptjs, bcrypt does not work on ubuntu

--- models/movies.js
+++ models/movies.js
@@ -1,49 +1,49 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const movieSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, "A movie must have a name"],
+    required: [true, 'A movie must have a name'],
     minLength: 2,
-    maxLength: 200,
+    maxLength: 200
   },
   year: {
-    type: String,
-    required: [true, "A movie must have a release year"],
+    type: Date,
+    required: [true, 'A movie must have a release year']
   },
   cast: {
     type: Array,
-    required: [true, "A movie must have casts"],
+    required: [true, 'A movie must have casts']
   },
   storyline: {
     type: String,
     minLength: 10,
     maxLength: 750,
-    default: "A description has not been added for this movie",
+    default: 'A description has not been added for this movie'
   },
   genre: {
     type: [String],
     enum: [
-      "action",
-      "adventure",
-      "comedy",
-      "fantasy",
-      "drama",
-      "fiction",
-      "thriller",
-      "horror",
-      "documentary",
-      "anime",
-      "family",
-      "crime",
-      "animation",
-    ],
+      'action',
+      'adventure',
+      'comedy',
+      'fantasy',
+      'drama',
+      'fiction',
+      'thriller',
+      'horror',
+      'documentary',
+      'anime',
+      'family',
+      'crime',
+      'animation'
+    ]
   },
   directors: {
-    type: [String],
-  },
-});
+    type: [String]
+  }
+})
 
-const Movie = mongoose.model("Movie", movieSchema);
+const Movie = mongoose.model('Movie', movieSchema)
 
-module.exports = Movie;
+module.exports = Movie
--- models/user.js
+++ models/user.js
@@ -1,71 +1,71 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 200,
-    required: [true, "A user must have a name"],
+    required: [true, 'A user must have a name']
   },
   email: {
     type: String,
-    required: [true, "A user must have an email address"],
+    required: [true, 'A user must have an email address'],
     unique: true,
     validate: {
       validator: function (val) {
-        return validator.isEmail(val);
+        return validator.isEmail(val)
       },
-      message: "Please provide a valid email address",
-    },
+      message: 'Please provide a valid email address'
+    }
   },
   password: {
     type: String,
-    required: [true, "user must have a password"],
+    required: [true, 'user must have a password'],
     minlength: 6,
-    select: false,
+    select: false
   },
   passwordConfirm: {
     type: String,
     validate: {
       validator: function (val) {
-        return this.password === val;
+        return this.password === val
       },
-      message: "Passwords do not match",
-    },
+      message: 'Passwords do not match'
+    }
   },
   passwordChangedAt: Date,
   role: {
     type: String,
-    enum: ["user", "admin", "creator"],
-    default: "user",
-  },
-});
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
+    enum: ['user', 'admin', 'creator'],
+    default: 'user'
   }
-  this.password = await bcrypt.hash(this.password, 10);
-  this.passwordConfirm = undefined;
-});
+})
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 10)
+  this.passwordConfirm = undefined
+})
 
 userSchema.methods.changedPasswordAfter = function (val) {
   if (this.passwordChangedAt) {
-    const timestamp = this.passwordChangedAt / 1000;
+    const timestamp = this.passwordChangedAt / 1000
 
-    return val < timestamp;
+    return val < timestamp
   }
 
-  return false;
-};
+  return false
+}
 
 userSchema.methods.comparePassword = async function (val) {
-  const auth = await bcrypt.compare(val, this.password);
+  const auth = await bcrypt.compare(val, this.password)
 
-  return auth;
-};
+  return auth
+}
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+const User = mongoose.model('User', userSchema)
+module.exports = User
